15f67f8bd1ac8daab8aba00f1b86e45b39d6ef8d
da-sasaki <da-sasaki@kentem.co.jp>
2022-10-13 21:53:00 +0900

    テンプレートのサンプル追加

---
+++ class.template/class.template.cpp
@@ -0,0 +1,39 @@
+﻿// class.template.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+
+template <typename T>
+class Rectangle {
+public:
+    Rectangle(T height, T width) 
+        : height_(height), width_(width) {}
+
+    T Area() const {
+        return height_ * width_;
+    }
+
+private:
+    const T height_;
+    const T width_;
+};
+
+int main()
+{
+    Rectangle<int> rect1(1, 2);
+    Rectangle<double> rect2(1.1, 2.0);
+    std::cout << rect1.Area() << std::endl;
+    std::cout << rect2.Area() << std::endl;
+    std::getchar();
+}
+
+// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
+// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+
+// 作業を開始するためのヒント: 
+//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
+//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
+//   4. エラー一覧ウィンドウを使用してエラーを表示します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
